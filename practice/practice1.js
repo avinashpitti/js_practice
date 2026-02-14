e1095323da615ccf8806a9b058b792a0c56a7082
avinashpitti <avinash87129@gmail.com>
2026-02-14 12:42:03 +0530

    escape characters in javascript

--- practice/practice1.js
+++ practice/practice1.js
@@ -1,28 +1,5 @@
-let obj1 = {
-  "name": "Avinash",
-  address: {
-    city: "Bangalore"
-  }
-}
-
-let obj2 = { ...obj1 }   // shallow copy
-
-obj2.name = "Rahul"
-obj2.address.city = "Delhi"
-
-console.log(obj1)
-console.log(obj2)
-
-let student={
-  st_name : 'Avinash',
-  age :25,
-  is_studnet : true
-}
-
-console.log(student.st_name);
-console.log(student['st_name'])
-console.log(student[st_name])
-console.log(student.age);
-console.log(student["age"]);
-console.log(student[age])
-
+console.log('hello\navinash');
+console.log('hello\tavinash')
+console.log("\"India\" is my country");
+console.log("India's my country")
+console.log('India\'s my country')
