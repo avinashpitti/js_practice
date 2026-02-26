5f95fe09f744347f0cc896c5c6e311e536c87a9f
avinashpitti <avinash87129@gmail.com>
2026-02-26 12:52:40 +0530

    dom practice

--- practice/practice2.js
+++ practice/practice2.js
@@ -1,5 +1,16 @@
-document.getElementById('demo').innerHTML='Hello, avinash,how are you doing?'
+// document.getElementById('demo').innerHTML='Hello, avinash,how are you doing?'
 
-document.querySelector('#imp').innerHTML='This is an important information'
+// document.querySelector('#imp').innerHTML='This is an important information'
 
-document.querySelector('#least').innerHTML='changing to queryselector'
+// document.querySelector('#least').innerHTML='changing to queryselector'
+
+document.getElementById('btn').onclick=function(){
+    // document.getElementById('h11').style.color='red';
+    // document.getElementById('h11').style.fontSize='60px';
+    // document.getElementById('h11').style.display='none';
+    document.getElementById('h11').style.visibility='hidden';
+
+}
+
+
+
